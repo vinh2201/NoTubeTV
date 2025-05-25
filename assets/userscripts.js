d2785b50b6d827b540ad833817c95cce63e69288
ycngmn <niloymollick90@gmail.com>
2025-05-25 08:41:22 +0200

    Fixed playback speed.

--- assets/userscripts.js
+++ assets/userscripts.js
@@ -91,6 +91,48 @@
 })();
 /* End menuTrigger.js */
 
+/* Start speedMenuTrigger.js */
+(function() {
+    function applySpeedMod() {
+        const speedBtn = document.querySelector('yt-icon.ytContribIconClockHalfCircle')?.closest('ytlr-button');
+        if (speedBtn) {
+            const speedBtnSub = speedBtn.querySelector('yt-formatted-string[idomkey="ytLrAvatarLockupSubtitle"]');
+            if (speedBtnSub) {
+                const customSub = document.createElement('span');
+                customSub.textContent = configRead('videoSpeed') + 'x';
+                customSub.className = speedBtnSub.className;
+                speedBtnSub.replaceWith(customSub);
+            }
+            if (!speedBtn.dataset.listenerAttached) {
+                speedBtn.dataset.listenerAttached = "true";
+                speedBtn.addEventListener('keydown', function(event) {
+                    if (event.key === 'Enter' || event.keyCode === 13) {
+                        event.preventDefault();
+                        loadSpeedMenu();
+                    }
+                });
+                speedBtn.addEventListener('click', () => {
+                    loadSpeedMenu();
+                });
+            }
+        }
+    }
+
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach(() => {
+            applySpeedMod();
+        });
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+
+    applySpeedMod();
+})();
+/* End speedMenuTrigger.js */
+
 /* Start exitBridge.js */
 // Exit Bridge to react to exit button call.
 (function () {
@@ -907,19 +949,21 @@
     localConfig = defaultConfig;
   }
 
-  function configRead(key) {
-    if (localConfig[key] === undefined) {
+  window.localConfig = window.localStorage[CONFIG_KEY]
+    ? JSON.parse(window.localStorage[CONFIG_KEY])
+    : defaultConfig;
 
-      localConfig[key] = defaultConfig[key];
+  window.configRead = function (key) {
+    if (window.localConfig[key] === undefined) {
+      window.localConfig[key] = defaultConfig[key];
     }
+    return window.localConfig[key];
+  };
 
-    return localConfig[key];
-  }
-
-  function configWrite(key, value) {
-    localConfig[key] = value;
-    window.localStorage[CONFIG_KEY] = JSON.stringify(localConfig);
-  }
+  window.configWrite = function (key, value) {
+    window.localConfig[key] = value;
+    window.localStorage[CONFIG_KEY] = JSON.stringify(window.localConfig);
+  };
 
   function parseTimestamps(input) {
       var lines = input.trim().split('\n');
@@ -4027,43 +4071,7 @@
       configWrite('routeColor', evt.target.value);
       updateStyle();
     });
-/*
-    var eventHandler = (evt) => {
-      // We handle key events ourselves.
 
-      if (evt.keyCode == 403) {
-        evt.preventDefault();
-        evt.stopPropagation();
-        if (evt.type === 'keydown') {
-          if (uiContainer.style.display === 'none') {
-            uiContainer.style.display = 'block';
-            uiContainer.focus();
-          } else {
-            uiContainer.style.display = 'none';
-            uiContainer.blur();
-          }
-        }
-        return false;
-      } else if (evt.keyCode == 404) {
-        if (evt.type === 'keydown') {
-          modernUI();
-        }
-      }    return true;
-    };
-
-    // Red, Green, Yellow, Blue
-    // 403, 404, 405, 406
-    // ---, 172, 170, 191
-    document.addEventListener('keydown', eventHandler, true);
-    document.addEventListener('keypress', eventHandler, true);
-    document.addEventListener('keyup', eventHandler, true);
-
-    setTimeout(() => {
-     showToast('Welcome to NotubeTv', 'Press [GREEN] to open NotubeTv Settings, press [BLUE] to open Video Speed Settings and press [RED] to open NotubeTv Theme Settings.');
-    }, 2000);
-
-    // Fix UI issues, again. Love, Googol.
-*/
     if (configRead('enableFixedUI')) {
       try {
         const observer = new MutationObserver((_, _2) => {
@@ -4088,105 +4096,91 @@
   function execute_once_dom_loaded_speed() {
       document.querySelector('video').addEventListener('canplay', () => {
           document.getElementsByTagName('video')[0].playbackRate = configRead('videoSpeed');    });
-
-      const eventHandler = (evt) => {
-          const currentSpeed = configRead('videoSpeed');
-          if (evt.keyCode == 406 || evt.keyCode == 191) {
-              evt.preventDefault();
-              evt.stopPropagation();
-              if (evt.type === 'keydown') {
-                  let selectedIndex = 0;
-                  const maxSpeed = 4;
-                  const increment = 0.25;
-                  const buttons = [];
-                  for (let speed = increment; speed <= maxSpeed; speed += increment) {
-                      buttons.push(
-                          buttonItem(
-                              { title: `${speed}x` },
-                              null,
-                              [
-                                  {
-                                      signalAction: {
-                                          signal: 'POPUP_BACK'
-                                      }
-                                  },
-                                  {
-                                      setClientSettingEndpoint: {
-                                          settingDatas: [
-                                              {
-                                                  clientSettingEnum: {
-                                                      item: 'videoSpeed'
-                                                  },
-                                                  intValue: speed.toString()
-                                              }
-                                          ]
-                                      }
-                                  }
-                              ]
-                          )
-                      );
-                      if (currentSpeed === speed) {
-                          selectedIndex = buttons.length - 1;
-                      }
-                  }
-
-                  buttons.push(
-                      buttonItem(
-                          { title: `Fix stuttering (1.0001x)` },
-                          null,
-                          [
-                              {
-                                  signalAction: {
-                                      signal: 'POPUP_BACK'
-                                  }
-                              },
-                              {
-                                  setClientSettingEndpoint: {
-                                      settingDatas: [
-                                          {
-                                              clientSettingEnum: {
-                                                  item: 'videoSpeed'
-                                              },
-                                              intValue: '1.0001'
-                                          }
-                                      ]
-                                  }
-                              }
-                          ]
-                      )
-                  );
-
-                  showModal('Playback Speed', buttons, selectedIndex, 'tt-speed');
-
-                  let observer = new MutationObserver((mutationsList, observer) => {
-                      let modal = null;
-                      const elements = document.getElementsByTagName('yt-formatted-string');
-                      for (const element of elements) {
-                          if (element.innerText === 'Playback Speed') {
-                              modal = element;
-                              break;
-                          }
-                      }
-                      if (!modal) {
-                          document.getElementsByTagName('video')[0].playbackRate = configRead('videoSpeed');
-                          observer.disconnect();
-                      }
-                  }
-                  );
-                  observer.observe(document.body, { childList: true, subtree: true });
-                  return false;
-              }
-              return true;
-          }    };
-
-      // Red, Green, Yellow, Blue
-      // 403, 404, 405, 406
-      // ---, 172, 170, 191
-      document.addEventListener('keydown', eventHandler, true);
-      document.addEventListener('keypress', eventHandler, true);
-      document.addEventListener('keyup', eventHandler, true);
   }
 
+  window.loadSpeedMenu = () => {
+      const currentSpeed = configRead('videoSpeed');
+
+      let selectedIndex = 0;
+      const maxSpeed = 4;
+      const increment = 0.25;
+      const buttons = [];
+      for (let speed = increment; speed <= maxSpeed; speed += increment) {
+          buttons.push(
+              buttonItem(
+                  { title: `${speed}x` },
+                  null,
+                  [
+                      {
+                          signalAction: {
+                              signal: 'POPUP_BACK'
+                          }
+                      },
+                      {
+                          setClientSettingEndpoint: {
+                              settingDatas: [
+                                  {
+                                      clientSettingEnum: {
+                                          item: 'videoSpeed'
+                                      },
+                                      intValue: speed.toString()
+                                  }
+                              ]
+                          }
+                      }
+                  ]
+              )
+          );
+          if (currentSpeed === speed) {
+              selectedIndex = buttons.length - 1;
+          }
+      }
+
+      buttons.push(
+          buttonItem(
+              { title: `Fix stuttering (1.0001x)` },
+              null,
+              [
+                  {
+                      signalAction: {
+                          signal: 'POPUP_BACK'
+                      }
+                  },
+                  {
+                      setClientSettingEndpoint: {
+                          settingDatas: [
+                              {
+                                  clientSettingEnum: {
+                                      item: 'videoSpeed'
+                                  },
+                                  intValue: '1.0001'
+                              }
+                          ]
+                      }
+                  }
+              ]
+          )
+      );
+
+      showModal('Playback Speed', buttons, selectedIndex, 'tt-speed');
+
+      let observer = new MutationObserver((mutationsList, observer) => {
+          let modal = null;
+          const elements = document.getElementsByTagName('yt-formatted-string');
+          for (const element of elements) {
+              if (element.innerText === 'Playback Speed') {
+                  modal = element;
+                  break;
+              }
+          }
+          if (!modal) {
+              document.getElementsByTagName('video')[0].playbackRate = configRead('videoSpeed');
+              observer.disconnect();
+          }
+      });
+      observer.observe(document.body, { childList: true, subtree: true });
+      return false;
+  }
 })();
 
 /* End TizenScrips.js */
