4b98696e843e0c14b3ce2f403a112465bbe9bd28
ycngmn <niloymollick90@gmail.com>
2025-05-28 17:27:24 +0200

    Added permission for voice search

--- app/src/main/java/com/ycngmn/notubetv/utils/managePerms.kt
+++ app/src/main/java/com/ycngmn/notubetv/utils/managePerms.kt
@@ -2,19 +2,13 @@ package com.ycngmn.notubetv.utils
 
 import android.content.Context
 import android.content.pm.PackageManager
-import android.util.Log
 import android.webkit.PermissionRequest
-import android.webkit.WebResourceError
-import android.webkit.WebResourceRequest
-import android.webkit.WebResourceResponse
-import android.webkit.WebView
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.core.content.ContextCompat
 import com.multiplatform.webview.web.AccompanistWebChromeClient
-import com.multiplatform.webview.web.AccompanistWebViewClient
 import com.multiplatform.webview.web.PlatformWebViewParams
 
 @Composable
@@ -23,31 +17,6 @@ fun permissionHandlerChrome(context: Context): PlatformWebViewParams {
     val permissionLauncher = rememberLauncherForActivityResult(
         ActivityResultContracts.RequestPermission()
     ) {}
-
-    val client = remember {
-        object : AccompanistWebViewClient() {
-            override fun onReceivedError(
-                view: WebView,
-                request: WebResourceRequest?,
-                error: WebResourceError?
-            ) {
-                super.onReceivedError(view, request, error)
-                Log.e("WebViewError", "Error loading URL: ${request?.url}")
-                Log.e("WebViewError", "Error code: ${error?.errorCode}, description: ${error?.description}")
-
-            }
-
-            override fun onReceivedHttpError(
-                view: WebView,
-                request: WebResourceRequest,
-                errorResponse: WebResourceResponse
-            ) {
-                super.onReceivedHttpError(view, request, errorResponse)
-                Log.e("WebViewHttpError", "HTTP error on URL: ${request.url}")
-                Log.e("WebViewHttpError", "Status: ${errorResponse.statusCode}, Reason: ${errorResponse.reasonPhrase}")
-            }
-        }
-    }
 
     val chrome = remember {
         object : AccompanistWebChromeClient() {
@@ -58,8 +27,7 @@ fun permissionHandlerChrome(context: Context): PlatformWebViewParams {
             }
         }
     }
-    return PlatformWebViewParams(
-        client = client,chromeClient = chrome)
+    return PlatformWebViewParams(chromeClient = chrome)
 }
 
 fun hasPermission(context: Context) : Boolean  {
